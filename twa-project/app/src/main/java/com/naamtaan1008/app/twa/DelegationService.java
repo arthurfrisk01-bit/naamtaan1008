1aaedf37ef3edaab631fd49b3ddb570287da13f2
Boner <boner@teacross.cc>
2026-08-12 16:38:18 +0800

    feat: TWA 项目模板 + CI 直接 build（config 预写跳过交互，密码走环境变量）

---
+++ twa-project/app/src/main/java/com/naamtaan1008/app/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.naamtaan1008.app.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
